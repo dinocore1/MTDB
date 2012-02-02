49e3072af156537cc902aff9e8227ad3af4febf0
Paul Soucy <paul@dev-smart.com>
2012-02-01 21:02:58 -0500

    initial project creation

---
+++ MTDB/MTResultSet.h
@@ -0,0 +1,13 @@
+//
+//  MTResultSet.h
+//  MTDB
+//
+//  Created by Paul Soucy on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface MTResultSet : NSObject
+
+@end
---
+++ MTDB/MTResultSet.m
@@ -0,0 +1,13 @@
+//
+//  MTResultSet.m
+//  MTDB
+//
+//  Created by Paul Soucy on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MTResultSet.h"
+
+@implementation MTResultSet
+
+@end
