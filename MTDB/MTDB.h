a11eb0a11fb34c0d38f71c357683dfb32967b00d
Paul Soucy <paul@dev-smart.com>
2012-02-01 20:21:13 -0500

    Initial Commit

---
+++ MTDB/MTDB.h
@@ -0,0 +1,13 @@
+//
+//  MTDB.h
+//  MTDB
+//
+//  Created by Paul Soucy on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MTDB : NSObject
+
+@end
---
+++ MTDB/MTDB.m
@@ -0,0 +1,23 @@
+//
+//  MTDB.m
+//  MTDB
+//
+//  Created by Paul Soucy on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MTDB.h"
+
+@implementation MTDB
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
